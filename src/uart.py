817bdebc5dcef8aea70db02412b9a38410868e34
MaximCore007 <malyshenko_mv@teslatec.ru>
2023-03-03 10:45:24 +0700

    added input params
    - list of available ports
    - number samples
    - timeout between samples

--- src/uart.py
+++ src/uart.py
@@ -2,23 +2,23 @@
 
 import threading
 import serial
+import serial.tools.list_ports
 import minimalmodbus
-from minimalmodbus import Instrument
-from asyncio.tasks import sleep
 
 SLAVE_ID = 1
 
 isRequest = 0
 isExit = 0
     
-def uart_task(event):
+def uart_task(event, com, num_smpl, time):
     # tmr = threading.Timer(0.5, hello)
     # tmr.start()
     # ser = serial.Serial() 
     # ser.baudrate = 115200
     # ser.port = 'COM10'
 
-    instrument = minimalmodbus.Instrument('COM12', 1)  # port name, slave address (in decimal)
+    print(com)
+    instrument = minimalmodbus.Instrument(com, 1)  # port name, slave address (in decimal)
     ser_name = instrument.serial.port                  # this is the serial port name               
     instrument.serial.baudrate = 115200                # Baud
     instrument.serial.bytesize = 8
@@ -37,52 +37,63 @@ def uart_task(event):
         full_name = "registers_41and42.csv"
         file1 = open(full_name, 'w', encoding="utf-8")
         print("Open file: "+full_name)
-
-        for i in range(100):
+        print()
+        for i in range(num_smpl):
             Reg41 = instrument.read_register(41)        # Registernumber, number of decimals
             Reg42 = instrument.read_register(42)        # Registernumber, number of decimals
             values_reg = f"%1d;%1d\n" %(Reg41, Reg42) 
-            print(values_reg)
             print(f"Reade data Register 41: %1d" %Reg41)
             print(f"Reade data Register 42: %1d" %Reg42)
             print()
             file1.write(values_reg)
-            event.wait(1)
+            event.wait(time)
             
         file1.close()
     else:
         print("Serial port is not open :(")
 
-    print("goodbye MAN!")
+    print(f"Данные записаны в файл {full_name}, количество выборок {num_smpl}, с интервалом {time} с.")
 
-def key_read():
-    print("---------------------------------")
-    print("b - begin request")
-    print("s - stop request")
-    print("x - exit")
-    k = input()
-    return k
-
-def keyRead_task():
-    key = key_read()
-
-    while True:
-        if (key == 'b'):
-            isRequest = 1
-        elif (key == 's'):
-            isRequest = 0
-        elif (key == 'x'):
-            isExit = 1
+# def key_read():
+#     print("---------------------------------")
+#     print("b - begin request")
+#     print("s - stop request")
+#     print("x - exit")
+#     k = input()
+#     return k
+#
+# def keyRead_task():
+#     key = key_read()
+#
+#     while True:
+#         if (key == 'b'):
+#             isRequest = 1
+#         elif (key == 's'):
+#             isRequest = 0
+#         elif (key == 'x'):
+#             isExit = 1
 
 
 def main():
-    # mutex = threading.Lock()
+    mutex = threading.Lock()
     event = threading.Event()
     
-    print("start threads...")
-    uart = threading.Thread(target=uart_task, args=(event,))
-    # keyrd = threading.Thread(target=keyRead_task)
+    ports = serial.tools.list_ports.comports()
+    print("Доступные COM-порты:")
+    for port, desc, hwid in sorted(ports):
+        print("Имя: {}; Описание: {}.".format(port, desc))
+    print("Введите имя COM-порта:")
+    com_port = str(input())    
+    print("Введите кол-во выборок:")
+    num_samples = int(input())
+    print("Введите задержку между выборками, мс.:")
+    timeout = float(input()) / 1000.0
 
+    print(com_port)
+    # print("start threads...")
+    uart = threading.Thread(target=uart_task, args=(event, com_port, num_samples, timeout))
+    # keyrd = threading.Thread(target=keyRead_task)
+    
     uart.start()
     # keyrd.start()
     
